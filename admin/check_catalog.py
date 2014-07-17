d0cc79e98185318078aaaeaadccf704d16d1315a
comtbot <comtbot@gmail.com>
2014-07-17 09:16:30 -0500

    rejecting maximum water level

--- admin/check_catalog.py
+++ admin/check_catalog.py
@@ -28,7 +28,41 @@ catalog='http://comt.sura.org/thredds/comt_1_archive_summary.xml'
 #c = Crawl(catalog, select=[".*-Agg"])
 c = Crawl(catalog)
 
-print c.datasets
+# <codecell>
+
+name_list=['water_surface_height_above_reference_datum',
+    'sea_surface_height_above_geoid','sea_surface_elevation',
+    'sea_surface_height_above_reference_ellipsoid','sea_surface_height_above_sea_level',
+    'sea_surface_height','water level']
+
+# <codecell>
+
+def name_in_list(cube):
+    return cube.standard_name in name_list
+
+# <codecell>
+
+name_constraint = iris.Constraint(cube_func=name_in_list)
+
+# <codecell>
+
+def my_test(cube):
+    return cube.standard_name in name_list & cube.cell_methods[0].method=='point'
+
+# <codecell>
+
+def my_func(cube):
+    b = False
+    if cube.standard_name in name_list:
+        if hasattr(cube,'cell_methods'):
+            b = not any(m.method == 'maximum' for m in cube.cell_methods)
+        else:
+            b = True
+    return b
+
+# <codecell>
+
+my_constraint = iris.Constraint(cube_func=my_func)
 
 # <codecell>
 
@@ -37,18 +71,18 @@ dap_urls = [s.get("url") for d in c.datasets for s in d.services if s.get("servi
 # <codecell>
 
 ugrid=[]
+not_ugrid=[]
 cf=[]
 not_cf=[]
 for url in dap_urls:
-    print url
     try:
         ug = pyugrid.UGrid.from_ncfile(url)
         ugrid.append(url)
     except:
-        print 'not a ugrid'
+        not_ugrid.append(url)
 
     try:
-        cube = iris.load_cube(url,'sea_surface_height_above_geoid')
+        cube = iris.load_cube(url,my_constraint)
         cf.append(url)
     except:
         not_cf.append(url)
@@ -59,12 +93,13 @@ len(ugrid)
 
 # <codecell>
 
+len(not_ugrid)
+
+# <codecell>
+
 len(cf)
 
 # <codecell>
 
 len(not_cf)
-
-# <codecell>
-
 
